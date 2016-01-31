ec46ecfdb912d0af9ee547e3eccfb658a2b0dc7a
buidanhquy <quy.buidanhquy@gmail.com>
2016-01-31 20:45:44 +0700

    add change (sunday night)

--- src/permission.c
+++ src/permission.c
@@ -92,7 +92,7 @@ void load_passphrase_db(passphrase_table_t *pph_table) {
     //==================== Open database ================================//   
     CALL_SQLITE (open (db_path, &db));
 
-    n = sprintf(sql ,"SELECT *FROM passphrase");
+    n = sprintf(sql ,"SELECT * FROM passphrase");
     sql[n] = '\0';
     CALL_SQLITE (prepare_v2 (db, sql, strlen (sql) + 1, &stmt, NULL));
     //===================== LOAD DATABASE (LEFT TO RIGHT) =====================================//
@@ -193,130 +193,130 @@ static int value_cmp(entry_t *n1, entry_t *n2) {
 }
 
 //======================== update table ==================================//
-void add_passphrase(passphrase_table_t *pph_table, char *field_1, char *field_2) {
+void add_passphrase(passphrase_table_t *pph_table, char *node_id, char *passphrase) {
     opool_item_t *item = NULL, *item2 = NULL;
 
     entry_t *temp, *temp2, *to_return;
 
     entry_t *data_list = NULL;
 
-    item = opool_get(&database->opool);
+    item = opool_get(&pph_table->opool);
     EXIT_IF_TRUE(item == NULL, "Cannot get from object pool\n");
     temp = (entry_t *)item->data;
 
-    printf("UPDATE TABLE passphrase VALUES('%s', '%s')\n", field_1, field_2);
+    printf("UPDATE TABLE passphrase VALUES('%s', '%s')\n", node_id, passphrase);
 
-    ansi_copy_str(temp->value, field_2);
+    ansi_copy_str(temp->value, passphrase);
 
-    data_list = (entry_t *)ht_get_item(&database->ht, (void *)field_1);
+    data_list = (entry_t *)ht_get_item(&pph_table->ht, (void *)node_id);
 
     if (data_list == NULL) {
-        item2 = opool_get(&database->opool);
+        item2 = opool_get(&pph_table->opool);
         EXIT_IF_TRUE(item2 == NULL, "Cannot get from object pool\n");
         temp2 = (entry_t *)item2->data;
 
-        ansi_copy_str(temp2->value, field_1);
+        ansi_copy_str(temp2->value, node_id);
 
-        DL_APPEND(database->key_list, temp2);
+        DL_APPEND(pph_table->key_list, temp2);
 
         DL_APPEND(data_list, temp);
-        ht_add_item(&database->ht, (void *)field_1, (void *)data_list);    
+        ht_add_item(&pph_table->ht, (void *)node_id, (void *)data_list);    
     }
     else {
         DL_SEARCH(data_list, to_return, temp, value_cmp);
         if (to_return != NULL) {
-            SHOW_LOG(3, "Record: %s - %s has already in the table passphrase!\n", field_1, field_2);
+            SHOW_LOG(3, "Record: %s - %s has already in the table passphrase!\n", node_id, passphrase);
         }
         else {
                 DL_REPLACE_ELEM(data_list, data_list, temp);
-                ht_add_item(&database->ht, (void *)field_1, (void *)data_list);    
+                ht_add_item(&database->ht, (void *)node_id, (void *)data_list);    
             }
     }
 }
 
-void add_new_account(login_table_t *l_table, char *field_1, char *field_2) {
+void add_active_acc(login_table_t *l_table, char *user_id, char *node_id) {
     opool_item_t *item = NULL, *item2 = NULL;
 
     entry_t *temp, *temp2, *to_return;
 
     entry_t *data_list = NULL;
 
-    item = opool_get(&database->opool);
+    item = opool_get(&l_table->opool);
     EXIT_IF_TRUE(item == NULL, "Cannot get from object pool\n");
     temp = (entry_t *)item->data;
 
-    printf("UPDATE TABLE login VALUES('%s', '%s')\n", field_1, field_2);
+    printf("UPDATE TABLE login VALUES('%s', '%s')\n", user_id, node_id);
 
-    ansi_copy_str(temp->value, field_2);
+    ansi_copy_str(temp->value, node_id);
 
-    data_list = (entry_t *)ht_get_item(&database->ht, (void *)field_1);
+    data_list = (entry_t *)ht_get_item(&l_table->ht, (void *)user_id);
 
     if (data_list == NULL) {
-        item2 = opool_get(&database->opool);
+        item2 = opool_get(&l_table->opool);
         EXIT_IF_TRUE(item2 == NULL, "Cannot get from object pool\n");
         temp2 = (entry_t *)item2->data;
 
-        ansi_copy_str(temp2->value, field_1);
+        ansi_copy_str(temp2->value, user_id);
 
-        DL_APPEND(database->key_list, temp2);
+        DL_APPEND(l_table->key_list, temp2);
 
         DL_APPEND(data_list, temp);
-        ht_add_item(&database->ht, (void *)field_1, (void *)data_list);    
+        ht_add_item(&l_table->ht, (void *)user_id, (void *)data_list);    
     }
     else {
         DL_SEARCH(data_list, to_return, temp, value_cmp);
         if (to_return != NULL) {
-            SHOW_LOG(3, "Record: %s - %s has already in the table login!\n", field_1, field_2);
+            SHOW_LOG(3, "Record: %s - %s has already in the table login!\n", user_id, node_id);
         }
         else {
                 DL_REPLACE_ELEM(data_list, data_list, temp);
-                ht_add_item(&database->ht, (void *)field_1, (void *)data_list);    
+                ht_add_item(&l_table->ht, (void *)user_id, (void *)data_list);    
             }
     }
 }
-void grant_access(permission_table_t *p_table, char *field_1, char *field_2) {
+void grant_access(permission_table_t *p_table, char *user_id, char *node_id) {
     opool_item_t *item = NULL, *item2 = NULL;
 
     entry_t *temp, *temp2, *to_return;
 
     entry_t *data_list = NULL;
 
-    item = opool_get(&database->opool);
+    item = opool_get(&p_table->opool);
     EXIT_IF_TRUE(item == NULL, "Cannot get from object pool\n");
     temp = (entry_t *)item->data;
 
-    printf("UPDATE TABLE permission VALUES('%s', '%s')\n", field_1, field_2);
+    printf("UPDATE TABLE permission VALUES('%s', '%s')\n", user_id, node_id);
 
-    ansi_copy_str(temp->value, field_2);
+    ansi_copy_str(temp->value, node_id);
 
-    data_list = (entry_t *)ht_get_item(&database->ht, (void *)field_1);
+    data_list = (entry_t *)ht_get_item(&p_table->ht, (void *)user_id);
 
     if (data_list == NULL) {
-        item2 = opool_get(&database->opool);
+        item2 = opool_get(&p_table->opool);
         EXIT_IF_TRUE(item2 == NULL, "Cannot get from object pool\n");
         temp2 = (entry_t *)item2->data;
 
-        ansi_copy_str(temp2->value, field_1);
+        ansi_copy_str(temp2->value, user_id);
 
-        DL_APPEND(database->key_list, temp2);
+        DL_APPEND(p_table->key_list, temp2);
 
         DL_APPEND(data_list, temp);
-        ht_add_item(&database->ht, (void *)field_1, (void *)data_list);    
+        ht_add_item(&p_table->ht, (void *)user_id, (void *)data_list);    
     }
     else {
         DL_SEARCH(data_list, to_return, temp, value_cmp);
         if (to_return != NULL) {
-            SHOW_LOG(3, "Record: %s - %s has already in the table permission!\n", field_1, field_2);
+            SHOW_LOG(3, "Record: %s - %s has already in the table permission!\n", user_id, node_id);
         }
         else {
                 DL_APPEND(data_list, temp);
-                ht_add_item(&database->ht, (void *)field_1, (void *)data_list);    
+                ht_add_item(&p_table->ht, (void *)user_id, (void *)data_list);    
             }
     }
 
     //REFLEX !!!
 #if 1
-    printf("UPDATE TABLE permission VALUES('%s', '%s')\n", field_2, field_1);
+    printf("UPDATE TABLE permission VALUES('%s', '%s')\n", node_id, user_id);
     data_list = NULL;
     to_return = NULL;
 
@@ -324,30 +324,30 @@ void grant_access(permission_table_t *p_table, char *field_1, char *field_2) {
     EXIT_IF_TRUE(item == NULL, "Cannot get from object pool\n");
     temp = (entry_t *)item->data;
 
-    ansi_copy_str(temp->value, field_1);
+    ansi_copy_str(temp->value, user_id);
 
-    data_list = (entry_t *)ht_get_item(&database->ht, (void *)field_2);
+    data_list = (entry_t *)ht_get_item(&p_table->ht, (void *)node_id);
 
     if (data_list == NULL) {
-        item2 = opool_get(&database->opool);
+        item2 = opool_get(&p_table->opool);
         EXIT_IF_TRUE(item2 == NULL, "Cannot get from object pool\n");
         temp2 = (entry_t *)item2->data;
 
-        ansi_copy_str(temp2->value, field_2);
+        ansi_copy_str(temp2->value, node_id);
 
-        DL_APPEND(database->key_list, temp2);
+        DL_APPEND(p_table->key_list, temp2);
 
         DL_APPEND(data_list, temp);
-        ht_add_item(&database->ht, (void *)field_2, (void *)data_list);    
+        ht_add_item(&p_table->ht, (void *)node_id, (void *)data_list);    
     }
     else {
         DL_SEARCH(data_list, to_return, temp, value_cmp);
         if (to_return != NULL) {
-            SHOW_LOG(3, "Record: %s - %s has already in the table passphrase!\n", field_2, field_1);
+            SHOW_LOG(3, "Record: %s - %s has already in the table passphrase!\n", node_id, user_id);
         }
         else {
                 DL_APPEND(data_list, temp);
-                ht_add_item(&database->ht, (void *)field_2, (void *)data_list);    
+                ht_add_item(&p_table->ht, (void *)node_id, (void *)data_list);    
             }
     }
 #endif
@@ -371,7 +371,7 @@ void save_login_database(login_table_t *l_table) {
     CALL_SQLITE_EXPECT (step (stmt), DONE);
 
     DL_FOREACH_SAFE(database->key_list, temp, entry) {
-        value_list = (entry_t *)ht_get_item(&database->ht, (void *)temp->value);
+        value_list = (entry_t *)ht_get_item(&l_table->ht, (void *)temp->value);
         DL_FOREACH_SAFE(value_list, temp2, entry2) {
             n = sprintf(sql, "INSERT INTO login VALUES ('%s', '%s')", temp->value, temp2->value);              
             printf("%s\n", sql);
@@ -401,7 +401,7 @@ void save_passphrase_database(passphrase_table_t *pph_table) {
     CALL_SQLITE_EXPECT (step (stmt), DONE);
 
     DL_FOREACH_SAFE(database->key_list, temp, entry) {
-        value_list = (entry_t *)ht_get_item(&database->ht, (void *)temp->value);
+        value_list = (entry_t *)ht_get_item(&pph_table->ht, (void *)temp->value);
         DL_FOREACH_SAFE(value_list, temp2, entry2) {
             n = sprintf(sql, "INSERT INTO passphrase VALUES ('%s', '%s')", temp->value, temp2->value);              
             printf("%s\n", sql);
@@ -432,7 +432,7 @@ void save_permission_database(permission_table_t *p_table) {
 
     DL_FOREACH_SAFE(database->key_list, temp, entry) {
         if ( (strstr(temp->value, "OIUC") == NULL) && (strstr(temp->value, "RIUC") == NULL)) {
-            value_list = (entry_t *)ht_get_item(&database->ht, (void *)temp->value);
+            value_list = (entry_t *)ht_get_item(&p_table->ht, (void *)temp->value);
             DL_FOREACH_SAFE(value_list, temp2, entry2) {
                 n = sprintf(sql, "INSERT INTO permission VALUES ('%s', '%s')", temp->value, temp2->value);              
                 printf("%s\n", sql);
@@ -444,7 +444,42 @@ void save_permission_database(permission_table_t *p_table) {
 }
 #endif
 
-
+void remove_passphrase(passphrase_table_t *pph_table, char *node_id) {
+    entry *temp;
+    entry *data_list;
+    
+    ansi_copy_str(temp->value, node_id);
+    data_list = (entry_t *)ht_get_item(&pph_table->ht, (void *)node_id);
+    
+    //List has only one element
+    DL_DELETE(data_list, data_list);
+    
+    DL_DELETE(pph_table->key_list, temp);
+}
+void revoke_access(permission_table_t *p_table, char *user_id, char *node_id) {
+    entry *temp, *temp2;
+    entry *data_list;
+    
+    ansi_copy_str(temp->value, user_id);
+    ansi_copy_str(temp2->value, node_id);
+    
+    data_list = (entry_t *)ht_get_item(&p_table->ht, (void *)user_id);
+    
+    DL_DELETE(data_list, temp2);
+    DL_DELETE(pph_table->key_list, temp);
+}
+void remove_active_acc(login_table_t *l_table, char *user_id) {
+    entry *temp;
+    entry *data_list;
+    
+    ansi_copy_str(temp->value, user_id);
+    data_list = (entry_t *)ht_get_item(&l_table->ht, (void *)user_id);
+   
+   //List has only one element 
+    DL_DELETE(data_list, data_list);
+    
+    DL_DELETE(l_table->key_list, temp);
+}
 
 void show_record(db_t *database, char *key) {
     entry_t *data_list;
